7185de0fd7fcae309c6bbef9ef11ddf5ac9459cf
Meera <mjvsharma@outlook.com>
2017-09-22 16:13:13 -0400

    created paperboy class, set 3 instnce variables within inittialize method and set up structure for quota, deliver, report methods

---
+++ ex4.rb
@@ -0,0 +1,25 @@
+class Paperboy
+
+def initialize(name)
+  @name = name
+  @experience = quota
+  @earnings= deliver
+
+end
+
+def quota
+
+
+
+end
+
+def deliver(start_address, end_address)
+
+
+end
+
+def report
+
+
+
+end
