19be9684ec03e814b6b4e121be714b3eb58583ed
Meera <mjvsharma@outlook.com>
2017-09-22 14:39:48 -0400

    made bank account method, and created instance methods for depositing and withdrawing from balance

--- ex1.rb
+++ ex1.rb
@@ -5,8 +5,27 @@ Every bank account should have balance and interest_rate attributes
 At this point you should test out creating an instance of your class to make sure it works
 =end
 
+=begin
+class Plant
+  def initialize(name, height)
+    @name = name
+    @height = height
+  end
+
+  def grow(inches)
+    @height += inches
+  end
+
+
+end
+
+puts doug = Plant.new("doug", 62)
+puts doug.grow(7)
+
+
+
 class BankAccount
-  def initialize()
+  def initialize
     @balance= balance
     @interest_rate = interest_rate
   end
@@ -20,4 +39,35 @@ class BankAccount
   end
 
 end
-  
+
+
+my_bank_account = BankAccount.new()
+my_bank_account.interest_rate(4)
+my_bank_account.balance(4000)
+
+
+=end
+
+
+
+ class BankAccount
+   def initialize
+     @balance=7000
+     @interest_rate
+   end
+
+   def deposit(amount)
+     @balance += amount
+   end
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+end
+
+
+my_account= BankAccount.new
+puts my_account
+
+puts my_account.deposit(900)
+puts my_account.withdraw(4200)
