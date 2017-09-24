665cb0eed29867bd5ea4ccb9aadf4a2e60af9e13
Meera <mjvsharma@outlook.com>
2017-09-24 11:30:32 -0400

    paperboy is working, but i seem to not understand why i get double quota statements printed when i call quota..commented it out but will ask and fix

--- ex4.rb
+++ ex4.rb
@@ -3,7 +3,7 @@ class Paperboy
 def initialize(name)
   @name = name
   @experience = 0
-  @earnings= deliver
+  @earnings= 0
 
 end
 
@@ -11,103 +11,53 @@ def quota
 #should calculate and return quota for next delivery
 #quota = 50+ 0.5*experience
 #first time
-minimum = 50
-quota = minimum + 0.5*@experience
-
-end
-
-def(start_address, end_address)
-  #provides the amount of money earned
-  house_number.to_f = end_address-start_address
-
-  if house_number > quota
-    earnings = (house_number -quota)*0.50 + house_number*0.25
-
-
-
-  #should update experience, aka the number of papers deliverd
-  @experience = @experience + house_number
-
-  return money earned
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  minimum = 50
+  quota = minimum + 0.5*@experience
+  #puts "#{quota}" when i use this line i get my statement printed out twice ?
+  quota
 
 end
 
 def deliver(start_address, end_address)
-days_houses=end_address - start_address
-earnings = days_houses*0.25
-  if days_houses > @experience
-    over_quota = 50 - days_houses
-    over_quota = 0.50*over_quota
-      return earnings +=over_quota
-  elsif days_houses <@experience
-    return earnings -=2
+  #provides the amount of money earned
+  house_number = end_address-start_address
+
+  if house_number >= quota
+     delivery_earnings = (house_number -quota)*0.50 + house_number*0.25
+     puts "Delivery earnings for exceeding quota: #{delivery_earnings}"
+  else
+    puts delivery_earnings= house_number*0.25 -2
+    puts "Delivery earnings: #{delivery_earnings}"
   end
 
-end
-
-#return amount earned as a float
-#update @experience
-
+  #should update experience, aka the number of papers deliverd
+  @experience = @experience + house_number
+  @earnings = @earnings + delivery_earnings
 
 end
 
-def report
+def earnings ()
+  puts "My total earnings are #{@earnings}"
+end
+
+def report()
 #needs to return a string related to performance
+puts "I am #{@name} and I have delivered #{@experience} papers and have earned $#{@earnings}!"
 
-puts "I am #{@name} and I have delivered #{papers} and have earned #{money}!"
-
+end
 
 end
 
 
-#min number of papers = 50 to deliver
-#qupta is 1/2 expereince + 50
-#therefore
-=begin
-1st time :
-50 papers
-2nd time:
-50 papes + 25
-50 papers + 75/2
-50 papers + 44
-quota therefore should take quota
+
+drake = Paperboy.new("Drake")
+drake.quota
+drake.deliver(20,40)
+drake.earnings
+drake.report
 
 
-
-
-=begin
-lets break down this problem so i can understand what the hell its asking
-there is a PAPERBOY. when we create paperboy instance from paperboy class,
-we are gonna provide the class paperboy with the argument "name" in order to give paperboy a name
-within the initialize method of the paperboy class, we are gonna have our 3 instance variables
-->name-->user provides this
-->experience-->the number of papers paperboy has delivered
-->earnings ---->the amount of money earned by paperboy
-
-we also have 3 instance methods in order to retrieve the status of the object's instance varables
-quota-->this method needs to tell us about the min number of papers required
-to
-deliver(start_address, end_address)
-report-->should puts out a statement of @name, @experience, @earnings
+drake.quota
+drake.deliver(75,300)
+drake.earnings
+drake.report
